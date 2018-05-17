8911bd9d6b6a047a3cb255cb7049a26cea349085
mjtr <mjtr1995@gmail.com>
2018-05-17 16:06:29 +0200

    casi todo ready

--- core/src/com/main/game/MyGdxGame.java
+++ core/src/com/main/game/MyGdxGame.java
@@ -107,12 +107,12 @@ public class MyGdxGame extends Game {
 
 
 
-		//loadingScreen = new LoadingScreen(this);
-		//setScreen(loadingScreen);
+		loadingScreen = new LoadingScreen(this);
+		setScreen(loadingScreen);
 
 
-		manager.finishLoading();
- 		setScreen(new GameLevel3Screen(this));
+		//manager.finishLoading();
+ 		//setScreen(new GameLevel3Screen(this));
 
 
 	}
